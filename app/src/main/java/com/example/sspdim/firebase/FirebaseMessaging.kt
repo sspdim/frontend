2b5d9cce67e23a3b26dd5ede2f573d71c057810b
Harivardhan-Swamy <swamyharivardhan@gmail.com>
2022-10-09 18:41:53 +0530

    Push Notification

--- app/src/main/java/com/example/sspdim/firebase/FirebaseMessaging.kt
+++ app/src/main/java/com/example/sspdim/firebase/FirebaseMessaging.kt
@@ -1,11 +1,21 @@
 package com.example.sspdim.firebase
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.widget.RemoteViews
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.example.sspdim.ChatInterface
+import com.example.sspdim.R
 import com.example.sspdim.network.AddFirebaseTokenRequest
 import com.example.sspdim.network.SspdimApi
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -14,6 +24,8 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 class FirebaseMessaging: FirebaseMessagingService() {
+    private val channelId = "Notif_Channel"
+    private val channelName = "com.example.sspdim.firebase"
 
     private var broadcaster: LocalBroadcastManager? = null
     /*private val database = Room.databaseBuilder(
@@ -147,5 +159,35 @@ class FirebaseMessaging: FirebaseMessagingService() {
                 e.printStackTrace()
             }
         }*/
+    }
+
+    private fun getRemoteView(title: String, message: String): RemoteViews {
+        val remoteView = RemoteViews("com.example.sspdim.firebase",R.layout.notification)
+        remoteView.setTextViewText(R.id.NotificationTitle,title)
+        remoteView.setTextViewText(R.id.NotificationContent,message)
+        remoteView.setImageViewResource(R.id.app_logo,R.drawable.notificationicon)
+        return remoteView
+    }
+
+    @SuppressLint("UnspecifiedImmutableFlag")
+    fun generateNotification(title: String, message:String){
+        val intent = Intent(this, ChatInterface::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT)
+        var builder : NotificationCompat.Builder = NotificationCompat.Builder(applicationContext,channelId)
+            .setSmallIcon(R.drawable.notificationicon)
+            .setAutoCancel(true)
+            .setVibrate(longArrayOf(1000,1000,1000,1000))
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+        builder = builder.setContent(getRemoteView(title,message))
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            val notificationChannel = NotificationChannel(channelId,channelName,NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        notificationManager.notify(0,builder.build())
     }
 }
