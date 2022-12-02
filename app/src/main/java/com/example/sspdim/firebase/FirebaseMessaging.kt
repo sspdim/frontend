f53b8af5d3b1e5e060047bcd527202bfe1361fb4
Dev <devvmashru@protonmail.ch>
2022-12-02 12:36:44 +0530

    Push Notification
    Co-authored-by: Harivardhan-Swamy <swamyharivardhan@gmail.com>
    Co-authored-by: DevMashru <devvmashru@protonmail.ch>

--- app/src/main/java/com/example/sspdim/firebase/FirebaseMessaging.kt
+++ app/src/main/java/com/example/sspdim/firebase/FirebaseMessaging.kt
@@ -1,14 +1,29 @@
 package com.example.sspdim.firebase
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.example.sspdim.MainActivity
+import com.example.sspdim.R
+import com.example.sspdim.data.SettingsDataStore
+import com.example.sspdim.model.SessionModel
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import kotlin.random.Random
 
 class FirebaseMessaging: FirebaseMessagingService() {
+    private val channelId = "Notif_Channel"
+    private val channelName = "com.example.sspdim.firebase"
 
     private var broadcaster: LocalBroadcastManager? = null
 
@@ -60,6 +75,13 @@ class FirebaseMessaging: FirebaseMessagingService() {
         handler.post(Runnable {
             message.data.let {
                 val intent = Intent("NewMessage")
+                val settingsDataStore = SettingsDataStore(applicationContext)
+                val username = runBlocking { settingsDataStore.usernamePreference.first() }
+                val server = runBlocking { settingsDataStore.serverPreference.first() }
+                val session = SessionModel("$username@$server")
+                val decryptedMessage = session.decryptNotification(applicationContext,message.data["message"]!!)
+                val from = message.data["data"]
+                generateNotification(from!!, decryptedMessage)
                 intent.putExtra("from", message.data["data"])
                 intent.putExtra("message", message.data["message"])
                 intent.putExtra("message_id", message.data["message_id"])
@@ -81,5 +103,29 @@ class FirebaseMessaging: FirebaseMessagingService() {
                 broadcaster?.sendBroadcast(intent)
             }
         })
+    }
+
+    fun generateNotification(title: String, message:String){
+        var randomGenerater = Random(System.currentTimeMillis() / 1000)
+        var notificationId = randomGenerater.nextInt(0, Int.MAX_VALUE)
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_IMMUTABLE)
+        var builder : NotificationCompat.Builder = NotificationCompat.Builder(applicationContext,channelId)
+            .setSmallIcon(R.drawable.notificationicon)
+            .setAutoCancel(true)
+            .setVibrate(longArrayOf(1000,1000,1000,1000))
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+            .setContentTitle(title)
+            .setContentText(message)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            val notificationChannel = NotificationChannel(channelId,channelName,NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        notificationManager.notify(notificationId,builder.build())
     }
 }
